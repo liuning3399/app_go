153c64b580950432439e0e1ff4605aff0da428af
root <441380237@qq.com>
2019-12-23 10:02:11 +0800

    44

--- website/manageweb/src/views/app/sys/test.vue
+++ website/manageweb/src/views/app/sys/test.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="app-container">
-    test
+    <div class="filter-container">
+      <el-input
+        v-model="listQuery.key"
+        placeholder="请输入内容"
+        clearable
+        prefix-icon="el-icon-search"
+        style="width: 200px;"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+        @clear="handleFilter"
+      />
+    </div>
   </div>
 </template>
